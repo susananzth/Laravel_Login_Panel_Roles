aa48a086d1e0f783b089b29d1019d1a1a2f0c0f6
susananzth <susananzth.dev@gmail.com>
2023-05-27 18:57:52 -0500

    Mejora en layout de invitado. Agregando barra de navegación

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -21,7 +21,7 @@ switch ($width) {
 }
 @endphp
 
-<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+<div {{ $attributes->merge(['class' => 'relative']) }} x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
     <div @click="open = ! open">
         {{ $trigger }}
     </div>
@@ -34,7 +34,7 @@ switch ($width) {
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
             class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;"
+            style="display: none; top:2.7rem"
             @click="open = false">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
             {{ $content }}
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'SusanaNzth') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,17 +14,63 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <body class="font-sans antialiased">
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+        <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex justify-between h-16">
+                    <div class="flex">
+                        <div class="shrink-0 flex items-center">
+                            <a href="{{ '/' }}">
+                                <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex">
+                        <x-dropdown align="right" width="48" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent
+                            text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700
+                            dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none
+                            focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700
+                            active:border-indigo-400 dark:active:border-indigo-600
+                            transition duration-150 ease-in-out">
+                            <x-slot name="trigger">
+                                <button>
+                                    <div>{{ __('Language') }}</div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Español') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('English') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                        @if (Route::has('login'))
+                            @auth
+                                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                    {{ __('Dashboard') }}
+                                </x-nav-link>
+                            @else
+                                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                                    {{ __('Log in') }}
+                                </x-nav-link>
+
+                                @if (Route::has('register'))
+                                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                                        {{ __('Register') }}
+                                    </x-nav-link>
+                                @endif
+                            @endauth
+                        @endif
+                    </div>
+                </div>
             </div>
+        </nav>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            {{ $slot }}
         </div>
     </body>
 </html>
