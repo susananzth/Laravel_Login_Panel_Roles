8ba0dd75f9f041f556491e80c4c90f7461a3b3a7
susananzth <susananzth.dev@gmail.com>
2023-05-27 21:02:49 -0500

    Cambiando colores de enlace, background, texto y botones en componentes

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => '
+    border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-pink-500
+    dark:focus:border-pink-600 focus:ring-pink-500 dark:focus:ring-pink-600 rounded-md shadow-sm']) !!}>
