3a4dfa06b4bff0809c8ed76dc6ce4cb53518e93f
susananzth <susananzth.dev@gmail.com>
2023-05-27 19:47:11 -0500

    Añadiendo logo. Mejora de padding en link de nav

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,7 +27,7 @@
                         </div>
                     </div>
                     <div class="flex">
-                        <x-dropdown align="right" width="48" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent
+                        <x-dropdown align="right" width="48" class="inline-flex items-center px-2 pt-1 border-b-2 border-transparent
                             text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700
                             dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none
                             focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700
