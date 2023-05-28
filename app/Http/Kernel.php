e4412d99710a44c7fd8e86fb310bebedb883a092
susananzth <susananzth.dev@gmail.com>
2023-05-28 00:28:09 -0500

    Añadiendo middleware de gates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,12 +37,14 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\AuthGates::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AuthGates::class,
         ],
     ];
 
---
+++ app/Http/Middleware/AuthGates.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+
+class AuthGates
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            $roles            = Role::with('permissions')->get();
+            $permissionsArray = [];
+
+            foreach ($roles as $role) {
+                foreach ($role->permissions as $permissions) {
+                    $permissionsArray[$permissions->title][] = $role->id;
+                }
+            }
+
+            foreach ($permissionsArray as $title => $roles) {
+                Gate::define($title, function ($user) use ($roles) {
+                    return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
+                });
+            }
+        }
+
+        return $next($request);
+    }
+}
