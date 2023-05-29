37216be11038459b30e7e8a180fbe97005b3ade5
susananzth <susananzth.dev@gmail.com>
2023-05-28 19:45:24 -0500

    Agregando seeder de roles y permisos

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            [
+                'id'    => 1,
+                'title' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id'    => 2,
+                'title' => 'User',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        Role::insert($roles);
+
+        $admin_permissions = Permission::all();
+        Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
+        $user_permissions = $admin_permissions->filter(function ($permission) {
+            $str = strpos($permission->title, 'index');
+            if ($str !== false) {
+                return true;
+            }else{
+                return false;
+            }
+        });
+        Role::findOrFail(2)->permissions()->sync($user_permissions);
+    }
+}
