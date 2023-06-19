31f65bd487d0da96f5ce296cbbc48f8f3fcc5bea
susananzth <susananzth.dev@gmail.com>
2023-06-18 19:33:10 -0500

    Instalando liveware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        @livewireStyles
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -40,5 +41,6 @@
                 </footer>
             </div>
         </div>
+        @livewireScripts
     </body>
 </html>
