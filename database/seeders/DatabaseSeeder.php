60b3e2c88caee56fd0b7958fc71053cde47b5474
susananzth <susananzth.dev@gmail.com>
2023-07-26 17:30:23 -0500

    Agregando backend de usuarios con controlador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,9 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
+            UserSeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
-        ]);
-        \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
     }
 }
