8046dc1653c37eb28b0bbb6bc06bd909d93a5ca4
susananzth <susananzth.dev@gmail.com>
2023-08-14 17:18:07 -0500

    Agregando funciones de editar y eliminar usuario

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -89,4 +89,97 @@ class Users extends Component
         session()->flash('message', trans('message.Created Successfully.', ['name' => __('User')]));
         session()->flash('alert_class', 'success');
     }
+
+    
+    public function edit($id)
+    {
+        if (Gate::denies('user_edit')) {
+            return redirect()->route('users')
+                ->with('message', trans('message.You do not have the necessary permissions to execute the action.'))
+                ->with('alert_class', 'danger');
+        }
+
+        $user = User::find($id);
+
+        if (!$user) {
+            session()->flash('error','User not found');
+            $this->emit('render');
+        } else {
+            $this->resetValidationAndFields();
+            $this->user_id    = $user->id;
+            $this->name       = $user->name;
+            $this->email      = $user->email;
+            $this->updateUser = true;
+            return view('user.edit');
+        }
+    }
+
+    public function update()
+    {
+        if (Gate::denies('user_edit')) {
+            return redirect()->route('users')
+                ->with('message', trans('message.You do not have the necessary permissions to execute the action.'))
+                ->with('alert_class', 'danger');
+        }
+
+        $this->validate();
+
+        DB::beginTransaction();
+        $user = User::find($this->user_id);
+        $user->name = $this->name;
+        $user->email = $this->email;
+        if (isset($this->password) || $this->password != '') {
+            $user->password = Hash::make($this->password);
+        }
+        $user->save();
+        DB::commit();
+
+        $this->resetValidationAndFields();
+        $this->emit('render');
+
+        session()->flash('message', trans('message.Updated Successfully.', ['name' => __('User')]));
+        session()->flash('alert_class', 'success');
+    }
+
+    public function cancel()
+    {
+        $this->resetValidationAndFields();
+    }
+
+    public function setDeleteId($id)
+    {
+        if (Gate::denies('user_delete')) {
+            return redirect()->route('users')
+                ->with('message', trans('message.You do not have the necessary permissions to execute the action.'))
+                ->with('alert_class', 'danger');
+        }
+
+        $user = User::find($id);
+        if (!$user) {
+            session()->flash('error','User not found');
+            $this->emit('render');
+        } else {
+            $this->user_id = $user->id;
+            $this->resetValidationAndFields();
+            $this->deleteUser = true;
+        }
+
+    }
+
+    public function delete()
+    {
+        if (Gate::denies('user_delete')) {
+            return redirect()->route('users')
+                ->with('message', trans('message.You do not have the necessary permissions to execute the action.'))
+                ->with('alert_class', 'danger');
+        }
+        DB::beginTransaction();
+        User::findOrFail($this->user_id)->delete();
+        DB::commit();
+        $this->resetValidationAndFields();
+        $this->emit('render');
+
+        session()->flash('message', trans('message.Deleted Successfully.', ['name' => __('User')]));
+        session()->flash('alert_class', 'success');
+    }
 }
